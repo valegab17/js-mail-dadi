62533c10a463d0a26ac1cfc2f37914f8388acf94
valeriagabrielli <gabvale53@gmail.com>
2025-12-04 20:48:26 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota: 
+Non è necessario provvedere alla validazione delle email */
+
