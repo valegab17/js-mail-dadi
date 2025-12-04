8984132f2b98984c6541220da81e49ba8ba5280a
valeriagabrielli <gabvale53@gmail.com>
2025-12-04 21:33:36 +0100

    esercizio mail completato

--- js/script.js
+++ js/script.js
@@ -8,3 +8,44 @@ Si può fare? Certo che si basta ragionare un po’.
 Nota: 
 Non è necessario provvedere alla validazione delle email */
 
+
+//devo creare una lista con gli invitati con un array quindi: 
+
+const mailList = [
+    `guestmail2@gmail.com`,
+    `guestmail1@gmail.com`,
+    `guestmail3@gmail.com`,
+    `guestmail4@gmail.com`,
+    `guestmail5@gmail.com`,
+    `guestmail6@gmail.com`,
+    `guestmail7@gmail.com`,
+    `guestmail8@gmail.com`,
+    `guestmail9@gmail.com`,
+    `guestmail10@gmail.com`,
+];
+
+//creo un prompt che chieda all'utente la sua email per verificare che faccia parte della lista 
+
+const guestMail = prompt("Inserisci qui la tua email.");
+
+
+//ancora non so se la mail è presente ma creo una variabile per l'esito 
+
+let found = false; //proprio perché non sappiamo se c'è
+
+//creo un ciclo della mia lista con FOR controllando le email una per una
+
+for(let i = 0; i < mailList.length; i++){
+    if(guestMail === mailList[i]){ //se corrisponde allora found sarà vero.
+        found = true;
+    }
+}
+
+//imposto il messaggio che verrà visualizzato nei due casi 
+const risultato = document.getElementById('risultato');
+if(found === true){
+    risultato.textContent = "Congratulazioni! La tua mail è nella lista!"
+}
+else{
+    risultato.textContent = "Spiacente, la tua mail non è nella lista."
+}
